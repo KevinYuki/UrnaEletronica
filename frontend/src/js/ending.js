544822410836c0c9894b15cae4801d59609a6e2d
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-09-25 19:13:26 -0300

    Tela operacional pronta e correção em Timeout da tela Ending

--- frontend/src/js/ending.js
+++ frontend/src/js/ending.js
@@ -1,1 +1,5 @@
-setTimeout(() => { window.location.href = "/"}, 5000 )
+const pathname =  window.location.pathname.substr(5)
+const name = pathname.substr(1,pathname.length-6)
+
+if(name === 'ending')
+setTimeout(() => { window.location.href = "/" }, 5000 )
