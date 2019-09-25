005c09f1929913085ea9376a704dd4ade8b0d3be
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-09-25 18:14:56 -0300

    Visual da tela de login feito

--- frontend/gulpTasks/deps.js
+++ frontend/gulpTasks/deps.js
@@ -2,8 +2,9 @@ const gulp = require('gulp')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
+const uglifycss = require('gulp-uglifycss')
 
-gulp.task('deps', ['deps.js'])
+gulp.task('deps', ['deps.js', 'deps.css'])
 
 gulp.task('deps.js', () => {
     return gulp.src([
@@ -13,4 +14,13 @@ gulp.task('deps.js', () => {
         .pipe(uglify())
         .pipe(concat('deps.min.js'))
         .pipe(gulp.dest('build/js'))
+})
+
+gulp.task('deps.css', () => {
+    return gulp.src([
+        'node_modules/bootstrap/dist/css/bootstrap.css'
+    ])
+        .pipe(uglifycss({ "uglyComments": true }))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/css'))
 })
