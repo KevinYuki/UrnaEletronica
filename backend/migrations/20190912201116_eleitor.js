e7c2194f11474e6c5e1aa44eb88cad56cee80d1f
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-09-27 22:41:30 -0300

    Tela insertUser populada

--- backend/migrations/20190912201116_eleitor.js
+++ backend/migrations/20190912201116_eleitor.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('eleitor', table => {
         table.increments('id').primary()
-        table.string('cpf').notNull()
+        table.string('cpf').notNull().unique()
         table.string('nome_completo').notNull().unique()
         table.string('cidade').notNull()
         table.string('estado').notNull()
